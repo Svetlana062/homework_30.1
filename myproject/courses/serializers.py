4569651cf37c534131e0c287127f856830dd6e2f
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-06-26 14:40:28 +0800

    Исправления с помощью black и flake8

--- myproject/courses/serializers.py
+++ myproject/courses/serializers.py
@@ -5,10 +5,10 @@ class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор модели Course для преобразования данных в формат JSON и обратно."""
     class Meta:
         model = Course
-        fields = ['id', 'name', 'preview', 'description']
+        fields = ['id', 'title', 'preview_image', 'description']
 
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор модели Lesson для преобразования данных в формат JSON и обратно."""
     class Meta:
         model = Lesson
-        fields = ['id', 'name', 'description', 'preview', 'video_link']
+        fields = ['id', 'course', 'title', 'description', 'preview_image', 'video_link']
--- myproject/courses/views.py
+++ myproject/courses/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, generics
+
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 
