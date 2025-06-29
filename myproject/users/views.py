f152f3f7991b6b1fd5c0947d2d7baecef14f7be3
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-06-30 02:28:27 +0800

    Added classes for payments and user profile

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,10 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 from rest_framework.permissions import IsAdminUser
+
+from ..courses.serializers import UserPaymentHistorySerializer
+
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     """ViewSet для модели CustomUser."""
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer  # сериализатор для преобразования данных
     permission_classes = [IsAdminUser]  # права доступа только у администратора
+
+
+class UserProfileView(generics.RetrieveAPIView):
+    """View для профиля пользователя."""
+    queryset = CustomUser.objects.all()
+    serializer_class = UserPaymentHistorySerializer
+
+    def get_object(self):
+        return self.request.user
