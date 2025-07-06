f70a1aba990114a7b0108a6ef97bdee01a68f0c2
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-07-07 02:36:23 +0800

    Added about owner field

--- myproject/courses/models.py
+++ myproject/courses/models.py
@@ -1,10 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+
+User = get_user_model()
 
 class Course(models.Model):
     """Модель курса обучения."""
     title = models.CharField(max_length=255, verbose_name='Название курса')
     preview_image = models.ImageField(upload_to='course_previews/', verbose_name='Превью (изображение) курса')
     description = models.TextField(verbose_name='Описание курса')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses', verbose_name='Владелец')
 
     def __str__(self):
         return self.title
@@ -20,6 +25,7 @@ class Lesson(models.Model):
     description = models.TextField(verbose_name='Описание урока')
     preview_image = models.ImageField(upload_to='lesson_previews/', verbose_name='Превью (изображение) урока')
     video_link = models.URLField(verbose_name='Ссылка на видео')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lessons', verbose_name='Владелец')
 
     def __str__(self):
         return f"{self.title} ({self.course.title})"
