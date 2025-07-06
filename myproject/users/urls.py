3cc85b2fcdd8fab5f140410827a57dcf3184414a
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-07-07 02:37:53 +0800

    New paths added

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserViewSet
-
+from .views import CustomUserViewSet, RegisterAPIView
 
 router = DefaultRouter()
 router.register(r'', CustomUserViewSet)
 
 urlpatterns = [
+    path('register/', RegisterAPIView.as_view(), name='register'),
     path('', include(router.urls)),
 ]
