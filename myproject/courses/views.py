3a0ea3c337f54c3756b9a8550980d49ce42c4905
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-07-20 01:21:39 +0800

    Изменения, данные наставником по поводу порядка urls и добавления настроек в конфигурацию.

--- myproject/courses/views.py
+++ myproject/courses/views.py
@@ -47,6 +47,7 @@ class CourseViewSet(viewsets.ModelViewSet):
 
 class LessonViewSet(viewsets.ModelViewSet):
     """ViewSet для урока (поддерживает все CRUD операции)."""
+
     queryset = Lesson.objects.all().order_by("id")
     serializer_class = LessonSerializer
     permission_classes = [
