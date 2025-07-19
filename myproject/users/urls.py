7c382d86b50d3d0442bb52519a4303f165fadd8b
Svetlana Kalashnikova <serebsveta@rambler.ru>
2025-07-20 01:18:01 +0800

    Create payment migration and add media

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, PaymentViewSet, RegisterAPIView
 
 router = DefaultRouter()
-router.register(r"", CustomUserViewSet, basename="customuserviewset")
 router.register(r"payments", PaymentViewSet, basename="payment")
+router.register(r"", CustomUserViewSet, basename="customuserviewset")
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
